67ad63193e1718a052d9048af1b7f94465e3f7f3
Emerson Moreira Neutzling <emerson.neutzling@gmail.com>
2023-06-14 09:12:59 -0300

    exercício jogo da velha

---
+++ jogodaVelha.js
@@ -0,0 +1,24 @@
+let tabuleiro
+let board
+let aviso
+let joagador
+let linha
+let coluna
+
+function iniciar(){
+    tabuleiro = []
+    board = document.getElementById('board')
+    aviso = document.getElementById('aviso')
+}
+
+function ixibir(){
+
+}
+
+function jogar(){
+
+}
+
+function checar(){
+    
+}
